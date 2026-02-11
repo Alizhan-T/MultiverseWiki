7bedeec7465c04657bffe143707c5a95e2f4314d
alzhnn <a.toleuxan06@mail.ru>
2026-02-11 22:41:00 +0500

    Initial Commit

---
+++ MultiverseWiki/MultiverseWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiverseWikiApp.swift
+//  MultiverseWiki
+//
+//  Created by Алижан Толеухан on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiverseWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiverseWikiTests/MultiverseWikiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiverseWikiTests.swift
+//  MultiverseWikiTests
+//
+//  Created by Алижан Толеухан on 11.02.2026.
+//
+
+import Testing
+@testable import MultiverseWiki
+
+struct MultiverseWikiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
